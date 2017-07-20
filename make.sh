1f2a88fbe3451f20392dbeb7e8e5215399f8d809
April Ayres <aag6581@gmail.com>
2017-07-21 00:07:08 +1000

    Produce zip files

--- make.sh
+++ make.sh
@@ -2,16 +2,26 @@
 
 ARCHES="darwin-amd64 windows-386 windows-amd64 linux-386 linux-amd64 linux-arm freebsd-arm freebsd-amd64 freebsd-386"
 BINDIR="bin"
+PUBLISH="publish"
 
 mkdir -p "$BINDIR"
+mkdir -p "$PUBLISH"
 
+exitState=0
 for arch in `echo $ARCHES`; do
 	export GOOS=`echo $arch | awk -F"-" '{print $1}'`
 	export GOARCH=`echo $arch | awk -F"-" '{print $2}'`
-	EXENAME="dskalyzer-$GOOS-$GOARCH"
+	EXENAME="$BINDIR/dskalyzer-$GOOS-$GOARCH"
+	ZIPNAME="$PUBLISH/dskalyzer-$GOOS-$GOARCH.zip"
 	if [ "$GOOS" == "windows" ]; then
 		EXENAME="$EXENAME.exe"
 	fi
-	echo "Building $BINDIR/$EXENAME..."
-	go build -o "$BINDIR/$EXENAME" .
+	echo "Building $EXENAME..."
+	go build -o "$EXENAME" .
+	if [ "$?" == "0" ]; then
+		echo "Zipping -> $ZIPNAME"
+		zip "$ZIPNAME" "$EXENAME" "LICENSE" "README.md"
+	else
+		exit 2
+	fi
 done
